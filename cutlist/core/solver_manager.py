80e7b5e572a0f5e5e3cead7cfedebb186fc20615
TheSmallHill <jacob.knoll16@gmail.com>
2022-06-19 11:56:05 -0500

    Renaming folders to follow pycharm guidelines

---
+++ cutlist/core/solver_manager.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+class SolverManager:
+    availableSolvers = []
+
+    def __init__(self):
+        # @todo implement
+        pass
+
+    def add_solver(self, solver):
+        # @todo implement
+        pass
+
+    def remove_solver(self, solver):
+        # @todo implement
+        pass
